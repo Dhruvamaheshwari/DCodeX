c053615c1d0cb9b1ce48b8126a74ce28d2dfd1e3
Dhruva Maheshwari <maheshwaridhruva0@gmail.com>
2026-05-11 19:19:13 +0530

    Start Frontend using Tailwind and daisyUi

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+
+
+
+function App() {
+  return (
+    <div className="bg-blue-100 p-8 text-center">
+      <h1 className="text-3xl font-bold ">Test Page</h1>
+      <button className="btn btn-primary mt-4">DaisyUI Button</button>
+    </div>
+  );
+}
+export default App;
